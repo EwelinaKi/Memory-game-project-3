d788bdcf3fe6e1995237f3f39adf82af1da9c248
Ewelina Kijanowska <kijanowska@gmail.com>
2018-03-22 22:07:06 +0100

    timer added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+var startTime, endTime;
+
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     let currentIndex = array.length,
@@ -34,21 +36,40 @@ function sumValues(map) {
 }
 
 function restartGame() {
+    //shuffle array and put new cards on deck
     shuffle(cardsArray);
     putCardsOnDeckFromShuffled(cardsArray);
-    checkMove = 0;
     //reset all values in cardsMAP
     for (let [key] of cardsMap) {
         cardsMap.set(key, 0);
     }
+    //reset numbers of moves
+    moves = 0;
+    checkMove = 0;
     //hide all cards
     const listOfCards = Array.from($(".card"));
-    listOfCards.forEach(resetAllCards); 
-    moves = 0;
+    listOfCards.forEach(resetAllCards);
+    //reset stars
     document.getElementById("moves").innerHTML = moves;
     document.getElementById("star1").setAttribute("class", "fa fa-star");
     document.getElementById("star2").setAttribute("class", "fa fa-star");
     document.getElementById("star3").setAttribute("class", "fa fa-star");
+    //set new timer
+    startTime = new Date();
+
+    // Update the count down every 1 second
+    var x = setInterval(function () {
+        // Get actual time
+        const now = new Date().getTime();
+        // Find the distance between start game and now
+        const distance = now - startTime;
+        // Time calculations for days, hours, minutes and seconds
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        // Output the result
+        document.getElementById("timer").innerHTML = minutes + ":" + seconds;
+    }, 1000);
+    return x
 }
 
 function hideCards(element, index, array) {
@@ -72,6 +93,15 @@ function updateScore(moves) {
     } else if (moves === 30) {
         document.getElementById("star3").setAttribute("class", "far fa-star");
     }
+}
+
+function displayModal(moves) {
+    endTime = new Date();
+    const fullStarsCount = Array.from(document.getElementsByClassName("fa fa-star")).length
+    const emptyStarsCount = Array.from(document.getElementsByClassName("far fa-star")).length
+    const totalTime = (endTime - startTime) / 60000;
+    document.getElementById("text").innerHTML = ("You earned " + "★".repeat(fullStarsCount) + "☆".repeat(emptyStarsCount) + "  with " + moves + " moves" + " in " + totalTime.toFixed(2) + " minutes.");
+    modal.style.display = "block";
 }
 
 
@@ -116,8 +146,9 @@ function playerMove(event) {
                 checkMove = 0;
                 //check the winnings
                 let sumOut = sumValues(cardsMap)
-                if (sumOut === 160) {
-                    //TODO: winnings POPUP
+                if (sumOut >= 0) {
+                    // if (sumOut === 160) {
+                    displayModal(totalMove)
                 }
                 //hide cards
                 const listOfCards = Array.from($(".card"));
@@ -129,7 +160,8 @@ function playerMove(event) {
 
 // prepare array of card classes to shuffle
 let cardsArray = ["fa-futbol-o", "fa-futbol-o", "fa-home", "fa-home", "fa-rocket", "fa-rocket", "fa-truck", "fa-truck", "fa-leaf", "fa-leaf",
-    "fa-puzzle-piece", "fa-puzzle-piece", "fa-binoculars", "fa-binoculars", "fa-birthday-cake", "fa-birthday-cake"];
+    "fa-puzzle-piece", "fa-puzzle-piece", "fa-binoculars", "fa-binoculars", "fa-birthday-cake", "fa-birthday-cake"
+];
 
 // caḍrsMap for tracking changes on dock for pair of cards: 0 - all cards down, 10 - one card's up, 20 - two cards up = match
 let cardsMap = new Map([
@@ -140,7 +172,8 @@ let cardsMap = new Map([
     ["fa fa-leaf", 0],
     ["fa fa-puzzle-piece", 0],
     ["fa fa-binoculars", 0],
-    ["fa fa-birthday-cake", 0]]);
+    ["fa fa-birthday-cake", 0]
+]);
 
 //the variable which counts rounds per move
 let checkMove = 0;
@@ -148,9 +181,19 @@ let checkMove = 0;
 //the variable which counts all rounds made by player
 let totalMove = 0;
 
-//add events listeners
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// Get the modal
+let modal = document.getElementById('myModal');
+
+//add events listeners:
 restartGame();
 $(".restart").click(restartGame);
 
 const card = $(".card");
 card.click(playerMove);
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
